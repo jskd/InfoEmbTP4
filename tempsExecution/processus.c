b7607e93f78b1d60047c4d123ece15b76b97bc18
jskd <git@jeromeskoda.fr>
2017-11-22 17:27:06 +0100

    remove mac support.

--- tempsExecution/processus.c
+++ tempsExecution/processus.c
@@ -4,11 +4,6 @@
 #include <time.h>
 #include <unistd.h>
 
-#ifdef __MACH__
-#include <mach/clock.h>
-#include <mach/mach.h>
-#endif
-
 #define NUMBER_OF_NS_IN_ONE_S 1000000000L
 #define NUMBER_OF_MS_IN_ONE_S 1000000L
 
@@ -18,49 +13,26 @@ double diff_time( struct timespec start, struct timespec end) {
 }
 
 void bench_processus(int max_processus) {
+
   int n_processus=0;
 
   struct timespec timeStart, timeEnd;
-  //clock_gettime(CLOCK_REALTIME, &timeStart);
-
-	#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
-		clock_serv_t cclock;
-		mach_timespec_t mts;
-		host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-		clock_get_time(cclock, &mts);
-		mach_port_deallocate(mach_task_self(), cclock);
-		timeStart.tv_sec = mts.tv_sec;
-		timeStart.tv_nsec = mts.tv_nsec;
-	#else
-		clock_gettime(CLOCK_REALTIME, &timeStart);
-	#endif
+  clock_gettime(CLOCK_REALTIME, &timeStart);
 
   while(n_processus <max_processus){
     pid_t pid=fork();
     if (pid == 0)
-    {
      exit(0);
-    }
     else if(pid > 0)
-    {
       n_processus++;
-    }
-    else
-    {
-      printf("fork() failed!\n");
+    else // fail
       break;
-    }
   }
 
-	#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
-		host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-		clock_get_time(cclock, &mts);
-		mach_port_deallocate(mach_task_self(), cclock);
-		timeEnd.tv_sec = mts.tv_sec;
-		timeEnd.tv_nsec = mts.tv_nsec;
-	#else
-		clock_gettime(CLOCK_REALTIME, &timeEnd);
-	#endif
+  clock_gettime(CLOCK_REALTIME, &timeEnd);
+
+  if(n_processus != max_processus)
+    printf("Attention: nombre d'échantillon non attein. (fork failed)\n");
 
   // moyenne en ms
   double moyenne= (diff_time(timeStart, timeEnd) / n_processus) * NUMBER_OF_MS_IN_ONE_S;
@@ -70,14 +42,18 @@ void bench_processus(int max_processus) {
 }
 
 int main (int argc, char **argv) {
-
   if(argc < 2) {
     printf("Usage: processus [nombre-echantillon]\n");
     exit(1);
-
   }
 
-  bench_processus(atoi(argv[1]));
+  int max_processus= atoi(argv[1]);
+  if(max_processus < 1) {
+    printf("Nombre-echantillon incorect.\n");
+    exit(1);
+  }
+
+  bench_processus(max_processus);
 
   exit(0);
 }
