f02b25bc22f831eff2a89da30b39bb1e9747b666
jskd <git@jeromeskoda.fr>
2017-11-23 23:02:54 +0100

    fix

--- tempsContext/thread.c
+++ tempsContext/thread.c
@@ -16,38 +16,34 @@ double diff_time( struct timespec start, struct timespec end) {
     +    ((double) end.tv_nsec-(double) start.tv_nsec) / NUMBER_OF_NS_IN_ONE_S;
 }
 
-void *thread_exit_func(){
-  pthread_exit(NULL);
-}
+void *context_change1(void* arg){
+  int max_context_change = (intptr_t) arg;
 
-void *thread1(void * n){
-  int nb = (int)n;
-
-  for(int i=0; i<nb; i++){
+  for(int n_context_change=0; n_context_change<max_context_change; n_context_change++){
     sem_wait(&sem_one);
     sem_post(&sem_two);
   }
   return NULL;
 }
 
-void *thread2(void * n){
-  int nb = (int)n;
+void *context_change2(void* arg){
+  int max_context_change = (intptr_t) arg;
 
-  for(int i=0; i<nb; i++){
+  for(int n_context_change=0; n_context_change<max_context_change; n_context_change++){
     sem_wait(&sem_two);
     sem_post(&sem_one);
   }
   return NULL;
 }
 
-void bench_thread(int max_thread) {
+void bench_context_change(int max_context_change) {
 
-  if(max_thread < 1 && max_thread > 10000) {
+  if(max_context_change < 1 && max_context_change > 10000) {
     return;
   }
 
-  int n_thread=0;
-  pthread_t t1, t2;
+  pthread_t t1;
+  pthread_t t2;
 
   if((sem_init(&sem_one, 1, 0)) == 1){
     perror("Error initializing semaphore");
@@ -62,38 +58,35 @@ void bench_thread(int max_thread) {
   struct timespec timeStart, timeEnd;
   clock_gettime(CLOCK_REALTIME, &timeStart);
 
-  pthread_create(&t1, NULL, thread1, (void *) n_thread);
-  pthread_create(&t2, NULL, thread2, (void *) n_thread);
+  pthread_create(&t1, NULL, context_change1, (void *) (intptr_t) max_context_change);
+  pthread_create(&t2, NULL, context_change2, (void *) (intptr_t) max_context_change);
 
   pthread_join(t1, NULL);
   pthread_join(t2, NULL);
 
   clock_gettime(CLOCK_REALTIME, &timeEnd);
 
-  if(n_thread != max_thread)
-    printf("Attention: nombre d'échantillon non attein. (pthread_create failed)\n");
-
   // moyenne en ms
-  double moyenne= (diff_time(timeStart, timeEnd) / n_thread) * NUMBER_OF_MS_IN_ONE_S;
+  double moyenne= (diff_time(timeStart, timeEnd) / max_context_change) * NUMBER_OF_MS_IN_ONE_S;
 
-  printf("Un thread (échantillon de %d) prend en moyenne: %lf ms.\n", n_thread, moyenne );
+  printf("Un context_change (échantillon de %d) prend en moyenne: %lf ms.\n", max_context_change, moyenne );
 
 }
 
 int main (int argc, char **argv) {
   if(argc < 2) {
-    printf("Usage: thread [nombre-echantillon]\n");
+    printf("Usage: context_change [nombre-echantillon]\n");
     exit(1);
   }
 
   // Pour eviter le débordement
-  int max_thread= atoi(argv[1]);
-  if(max_thread < 1 && max_thread > 10000) {
+  int max_context_change= atoi(argv[1]);
+  if(max_context_change < 1 && max_context_change > 10000) {
     printf("Nombre-echantillon incorect. (Entre 0 et 10000)\n");
     exit(1);
   }
 
-  bench_thread(max_thread);
+  bench_context_change(max_context_change);
 
   exit(0);
 }
