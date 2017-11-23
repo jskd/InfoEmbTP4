7e98186f28837689789684cfa2ef465411d5e1e6
CactusTribe <cactustribe@cactus.home>
2017-11-23 22:46:51 +0100

    Bug sur thread

--- tempsContext/thread.c
+++ tempsContext/thread.c
@@ -4,6 +4,9 @@
 #include <time.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
+
+sem_t sem_one, sem_two;
 
 #define NUMBER_OF_NS_IN_ONE_S 1000000000L
 #define NUMBER_OF_MS_IN_ONE_S 1000000L
@@ -17,6 +20,26 @@ void *thread_exit_func(){
   pthread_exit(NULL);
 }
 
+void *thread1(void * n){
+  int nb = (int)n;
+
+  for(int i=0; i<nb; i++){
+    sem_wait(&sem_one);
+    sem_post(&sem_two);
+  }
+  return NULL;
+}
+
+void *thread2(void * n){
+  int nb = (int)n;
+
+  for(int i=0; i<nb; i++){
+    sem_wait(&sem_two);
+    sem_post(&sem_one);
+  }
+  return NULL;
+}
+
 void bench_thread(int max_thread) {
 
   if(max_thread < 1 && max_thread > 10000) {
@@ -24,17 +47,26 @@ void bench_thread(int max_thread) {
   }
 
   int n_thread=0;
-  pthread_t pid;
+  pthread_t t1, t2;
+
+  if((sem_init(&sem_one, 1, 0)) == 1){
+    perror("Error initializing semaphore");
+    exit(1);
+  }
+
+  if((sem_init(&sem_two, 1, 1)) == 1){
+    perror("Error initializing semaphore");
+    exit(1);
+  }
 
   struct timespec timeStart, timeEnd;
   clock_gettime(CLOCK_REALTIME, &timeStart);
 
-  while(n_thread < max_thread){
-    if(pthread_create(&pid, NULL, thread_exit_func, NULL) != 0)
-      break;
-    else
-      n_thread++;
-  }
+  pthread_create(&t1, NULL, thread1, (void *) n_thread);
+  pthread_create(&t2, NULL, thread2, (void *) n_thread);
+
+  pthread_join(t1, NULL);
+  pthread_join(t2, NULL);
 
   clock_gettime(CLOCK_REALTIME, &timeEnd);
 
