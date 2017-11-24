0bb0a1a26e205e869b8035602904282adaf491bc
jskd <git@jeromeskoda.fr>
2017-11-24 23:01:05 +0100

    premier resultat stat

--- stat.py
+++ stat.py
@@ -8,11 +8,14 @@ import time
 
 lst= {}
 
-for i in range (500):
+for i in range (10000):
   out =  Popen(["taskset","-c", "0", "./expliciteFIFO/processus", "1000", "-o"], stdout=PIPE).communicate()[0].decode("utf-8")
-  out = round(float(out), 2)
-  lst[ str(out) ] += 1
+  out = str(round(float(out), 3)) + "ms"
+  if out in lst.keys() :
+    lst[out] = lst[out] + 1
+  else:
+    lst[out] = 1
 
-lst[out].sort()
+stat = sorted(lst.items())
 
-print(lst[out])
+print(stat)
