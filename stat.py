f1c5d4cf2c944b51e3e595497b1b39376448ec23
jskd <git@jeromeskoda.fr>
2017-11-24 23:49:22 +0100

    generation script

--- stat.py
+++ stat.py
@@ -8,18 +8,26 @@ import time
 
 
 
+def gen_table(cmd, iter):
+  lst= {}
 
-lst= {}
+  for i in range (iter):
+    out =  Popen(["taskset","-c", "0", cmd, "1000", "-o"], stdout=PIPE).communicate()[0].decode("utf-8")
+    out = str(round(float(out), 3))
+    if out in lst.keys() :
+      lst[out] = lst[out] + 1
+    else:
+      lst[out] = 1
 
-for i in range (10000):
-  out =  Popen(["taskset","-c", "0", "./expliciteFIFO/processus", "1000", "-o"], stdout=PIPE).communicate()[0].decode("utf-8")
-  out = str(round(float(out), 3)) 
-  if out in lst.keys() :
-    lst[out] = lst[out] + 1
-  else:
-    lst[out] = 1
+  stat = sorted(lst.items())
+  outputfile = cmd.replace("/", "").replace(".", "")
 
-stat = sorted(lst.items())
+  f = open("data/"+outputfile+".data", "w")
+  for temps, nombre in stat:
+    f.write( temps + " " + str(nombre)+"\n")
 
-for temps, nombre in stat:
-    print( temps + " " + str(nombre))
+
+gen_table("./expliciteFIFO/processus", 10000)
+gen_table("./expliciteFIFO/thread", 10000)
+gen_table("./implicteFIFO/processus", 10000)
+gen_table("./implicteFIFO/thread", 10000)
