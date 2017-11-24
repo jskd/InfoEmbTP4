e4940333ee1e0c0bfb787a35995a85061ab2cc65
jskd <git@jeromeskoda.fr>
2017-11-24 23:37:24 +0100

    Remove ms

--- stat.py
+++ stat.py
@@ -6,11 +6,14 @@ import time
 
 
 
+
+
+
 lst= {}
 
 for i in range (10000):
   out =  Popen(["taskset","-c", "0", "./expliciteFIFO/processus", "1000", "-o"], stdout=PIPE).communicate()[0].decode("utf-8")
-  out = str(round(float(out), 3)) + "ms"
+  out = str(round(float(out), 3)) 
   if out in lst.keys() :
     lst[out] = lst[out] + 1
   else:
@@ -18,4 +21,5 @@ for i in range (10000):
 
 stat = sorted(lst.items())
 
-print(stat)
+for temps, nombre in stat:
+    print( temps + " " + str(nombre))
