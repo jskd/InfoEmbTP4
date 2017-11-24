c09a3fd9c3a6f512193c0178f671b5e7beb628cf
jskd <git@jeromeskoda.fr>
2017-11-24 22:27:49 +0100

    Help

---
+++ stat.py
@@ -0,0 +1,10 @@
+
+from subprocess import *
+from os import listdir
+from os.path import isfile, join
+import time
+
+
+out =  Popen("taskset -c 0 ./expliciteFIFO/processus 10000 -o", stdout=PIPE).communicate()[0].decode("utf-8")
+
+print(out)
