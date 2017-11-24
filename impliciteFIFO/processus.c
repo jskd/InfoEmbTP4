f709fe0938910d4a51850dcc1ae94af34918a0e0
jskd <git@jeromeskoda.fr>
2017-11-24 15:02:13 +0100

    add implicit FIFO

---
+++ impliciteFIFO/processus.c
@@ -0,0 +1,79 @@
+/* Jérôme SKODA , Joaquim LEFRANC  TP° 4 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#define NUMBER_OF_NS_IN_ONE_S 1000000000L
+#define NUMBER_OF_MS_IN_ONE_S 1000000L
+
+double diff_time( struct timespec start, struct timespec end) {
+  return ((double) end.tv_sec -(double) start.tv_sec )
+    +    ((double) end.tv_nsec-(double) start.tv_nsec) / NUMBER_OF_NS_IN_ONE_S;
+}
+
+void bench_context_change(int max_context_change) {
+
+  if(max_context_change < 1 && max_context_change > 10000) {
+    return;
+  }
+
+  pid_t pid;
+
+  struct sched_param sched_param;
+  sched_param.sched_priority = 42; // Car c'est la réponse
+
+  max_context_change/= 2; // TODO: quick fix pour les deux changement de context fois 2
+
+  struct timespec timeStart, timeEnd;
+  clock_gettime(CLOCK_REALTIME, &timeStart);
+
+  if((pid = fork()) < 0) {
+    perror("Fork Failed");
+    exit(1);
+  }
+  else if(pid == 0){ // child
+    sched_setscheduler(getpid(), SCHED_FIFO, &sched_param);
+    for(int n_context_change=0; n_context_change<max_context_change; n_context_change++){
+      sched_yield();
+    }
+    exit(0);
+  }
+  else { // Parent
+    sched_setscheduler(getpid(), SCHED_FIFO, &sched_param);
+    for(int n_context_change=0; n_context_change<max_context_change; n_context_change++){
+      sched_yield();
+    }
+    waitpid(-1, NULL, 0);
+  }
+  clock_gettime(CLOCK_REALTIME, &timeEnd);
+
+  max_context_change*= 2; // TODO: quick fix pour les deux changement de context fois 2
+
+  // moyenne en ms
+  double moyenne= (diff_time(timeStart, timeEnd) / max_context_change) * NUMBER_OF_MS_IN_ONE_S;
+
+  printf("Un context_change (échantillon de %d) prend en moyenne: %lf ms.\n", max_context_change, moyenne );
+}
+
+int main (int argc, char **argv) {
+  if(argc < 2) {
+    printf("Usage: context_change [nombre-echantillon]\n");
+    exit(1);
+  }
+
+  // Pour eviter le débordement
+  int max_context_change= atoi(argv[1]);
+  if(max_context_change < 1 && max_context_change > 10000) {
+    printf("Nombre-echantillon incorect. (Entre 0 et 10000)\n");
+    exit(1);
+  }
+
+  bench_context_change(max_context_change);
+
+  exit(0);
+}
