751837016ac10c393088eebf707263e7e8a33a08
jskd <git@jeromeskoda.fr>
2017-11-24 23:55:13 +0100

    Final script

--- stat.py
+++ stat.py
@@ -4,20 +4,19 @@ from os import listdir
 from os.path import isfile, join
 import time
 
-
-
-
-
 def gen_table(cmd, iter):
   lst= {}
 
   for i in range (iter):
-    out =  Popen(["taskset","-c", "0", cmd, "1000", "-o"], stdout=PIPE).communicate()[0].decode("utf-8")
-    out = str(round(float(out), 3))
-    if out in lst.keys() :
-      lst[out] = lst[out] + 1
-    else:
-      lst[out] = 1
+    try:
+      out =  Popen(["taskset","-c", "0", cmd, "1000", "-o"], stdout=PIPE).communicate()[0].decode("utf-8")
+      out = str(round(float(out), 3))
+      if out in lst.keys() :
+        lst[out] = lst[out] + 1
+      else:
+        lst[out] = 1
+    except:
+      pass
 
   stat = sorted(lst.items())
   outputfile = cmd.replace("/", "").replace(".", "")
@@ -26,8 +25,11 @@ def gen_table(cmd, iter):
   for temps, nombre in stat:
     f.write( temps + " " + str(nombre)+"\n")
 
-
 gen_table("./expliciteFIFO/processus", 10000)
 gen_table("./expliciteFIFO/thread", 10000)
-gen_table("./implicteFIFO/processus", 10000)
-gen_table("./implicteFIFO/thread", 10000)
+gen_table("./impliciteFIFO/processus", 10000)
+gen_table("./impliciteFIFO/thread", 10000)
+gen_table("./tempsContext/processus", 10000)
+gen_table("./tempsContext/thread", 10000)
+gen_table("./tempsExecution/processus", 10000)
+gen_table("./tempsExecution/thread", 10000)
